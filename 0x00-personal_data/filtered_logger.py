e5624bd78291a850246a503821d9d01085a51bd3
Franklinson <franklinhassey@gmail.com>
2024-01-12 08:51:57 +0000

    return a log message

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 """ Handling personal data """
 
+from typing import List
 import re
 
 
-def filter_datum(fields: list[str], redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    return re.sub(fr'(?<=\b({"|".join(fields)})\b{separator})[^\{separator}]*',
-                  redaction, message)
+    """
+    Obfuscates fields in a log message using a single regex substitution.
+
+    Args:
+        fields: List of fields to obfuscate.
+        redaction: String to replace the fields with.
+        message: Log message to obfuscate.
+        separator: Separator character between fields in the log message.
+
+    Returns:
+          The obfuscated log message.
+          """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
