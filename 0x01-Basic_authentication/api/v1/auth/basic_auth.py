9474595fbdc75e9ba8163b3241670adf11ab0af8
Franklinson <franklinhassey@gmail.com>
2024-01-16 22:47:16 +0000

    implementation of basic64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,17 @@
 from api.v1.auth.auth import Auth
 
 
-
 class BasicAuth(Auth):
     """ Basic auth """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Implementing Basic64 """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        token = authorization_header.split(' ')[-1]
+        return token
