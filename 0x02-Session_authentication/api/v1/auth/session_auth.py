168318366ca052c21e70c6a0716f5c367ab7ea10
Franklinson <franklinhassey@gmail.com>
2024-01-19 00:22:04 +0000

    function to get user id from session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,3 +18,12 @@ class SessionAuth(Auth):
         session_id = uuid.uuid4()
         self.user_id_by_session_id[str(session_id)] = user_id
         return str(session_id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """function to get user_id"""
+        if session_id is None:
+            return None
+        if type(session_id) is not str:
+            return None
+        usr_id = self.user_id_by_session_id.get(session_id)
+        return usr_id
