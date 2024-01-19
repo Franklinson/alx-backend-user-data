09acb658c8e169a438b199b487a8688c1272a406
Franklinson <franklinhassey@gmail.com>
2024-01-19 00:18:13 +0000

    implementation of sesssion function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,3 +7,14 @@ import uuid
 
 class SessionAuth(Auth):
     """ Session auth class """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Session function"""
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[str(session_id)] = user_id
+        return str(session_id)
