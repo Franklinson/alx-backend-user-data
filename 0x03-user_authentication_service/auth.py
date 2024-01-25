df96dac3865c7ba8f101950f56e57ef6160034ed
Franklinson <franklinhassey@gmail.com>
2024-01-25 22:28:56 +0000

    valid user using auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,3 +29,12 @@ class Auth:
             pwd = _hash_password(password)
             user = self._db.add_user(email, pwd)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Validate user login """
+        try:
+            usr = self._db.find_user_by(email=email)
+            pwd = password.encode('utf-8')
+            return bcrypt.checkpw(pwd, usr.hashed_password)
+        except NoResultFound:
+            return False
