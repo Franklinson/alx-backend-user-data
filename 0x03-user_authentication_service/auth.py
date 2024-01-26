929ff2a8a217fd36abda5bc6f370df818d5489f6
Franklinson <franklinhassey@gmail.com>
2024-01-26 00:59:46 +0000

    reset password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -71,3 +71,13 @@ class Auth:
             return None
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """method to reset password token"""
+        try:
+            usr = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(usr.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
