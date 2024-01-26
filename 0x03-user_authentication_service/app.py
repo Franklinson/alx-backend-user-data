ff99612f2a40eac74cd3b5e2dada5a94554130c1
Franklinson <franklinhassey@gmail.com>
2024-01-26 01:06:35 +0000

    update user password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -81,5 +81,23 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """update user password"""
+    email = request.form.get('email', None)
+    reset_token = request.form.get('reset_token', None)
+    new_password = request.form.get('new_password', None)
+    if email is None or reset_token is None or new_password is None:
+        abort(403)
+    try:
+        actual_token = AUTH.get_reset_password_token(email)
+        if actual_token != reset_token:
+            abort(403)
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
